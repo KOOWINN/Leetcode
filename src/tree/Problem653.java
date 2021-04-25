49278c9713b7432026e050ac4624870c386798e0
koowinn <gooyunn@gmail.com>
2021-04-25 23:54:07 +0800

    Problem653:两数之和IV-输入BST

---
+++ src/tree/Problem653.java
@@ -0,0 +1,84 @@
+package tree;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * 653. 两数之和 IV - 输入 BST
+ * 给定一个二叉搜索树和一个目标结果，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+ *
+ * 案例 1:
+ *
+ * 输入:
+ *     5
+ *    / \
+ *   3   6
+ *  / \   \
+ * 2   4   7
+ *
+ * Target = 9
+ *
+ * 输出: True
+ *
+ *
+ * 案例 2:
+ *
+ * 输入:
+ *     5
+ *    / \
+ *   3   6
+ *  / \   \
+ * 2   4   7
+ *
+ * Target = 28
+ *
+ * 输出: False
+ */
+public class Problem653 {
+    // 深度优先遍历方法
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> set = new HashSet<>();
+        return dfs(root, k, set);
+    }
+
+    private boolean dfs(TreeNode root, int k, Set<Integer> set) {
+        if (root == null) {
+            return false;
+        }
+        if (set.contains(k - root.val)) {
+            return true;
+        }
+        set.add(root.val);
+        return dfs(root.left, k, set) || dfs(root.right, k, set);
+    }
+
+    // 广度优先遍历方法
+    public boolean findTarget2(TreeNode root, int k) {
+        Set<Integer> set = new HashSet<>();
+        return bfs(root, k, set);
+    }
+
+    private boolean bfs(TreeNode root, int k, Set<Integer> set) {
+        if (root == null) {
+            return false;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (set.contains(k - node.val)) {
+                return true;
+            }
+            set.add(root.val);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return false;
+    }
+}
