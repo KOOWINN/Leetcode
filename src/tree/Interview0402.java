1900638ee32b1844011648ac05bfa67004a08ada
koowinn <gooyunn@gmail.com>
2021-04-30 23:10:36 +0800

    Interview0402:最小高度树

---
+++ src/tree/Interview0402.java
@@ -0,0 +1,34 @@
+package tree;
+
+/**
+ * 面试题 04.02. 最小高度树
+ * 给定一个有序整数数组，元素各不相同且按升序排列，编写一个算法，创建一棵高度最小的二叉搜索树。
+ *
+ * 示例:
+ * 给定有序数组: [-10,-3,0,5,9],
+ *
+ * 一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+ *
+ *           0
+ *          / \
+ *        -3   9
+ *        /   /
+ *      -10  5
+ */
+public class Interview0402 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length <= 0) {
+            return null;
+        }
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (start > end) return null;
+        int mid = (start + end) >> 1;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums, start, mid - 1);
+        root.right = sortedArrayToBST(nums, mid + 1, end);
+        return root;
+    }
+}
