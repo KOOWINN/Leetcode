d01c4909b9c881d84472fd1ea5eea126408619cb
koowinn <gooyunn@gmail.com>
2021-05-06 21:53:44 +0800

    Problem83:删除排序链表中的重复元素

---
+++ src/linkedlist/Problem83.java
@@ -0,0 +1,41 @@
+package linkedlist;
+
+/**
+ * 83. 删除排序链表中的重复元素
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素只出现一次 。
+ *
+ * 返回同样按升序排列的结果链表。
+ *
+ *
+ *
+ * 示例 1：
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ *
+ * 示例 2：
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ *
+ *
+ * 提示：
+ *
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 <= Node.val <= 100
+ * 题目数据保证链表已经按升序排列
+ */
+public class Problem83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode current = head;
+        while (current.next!=null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
