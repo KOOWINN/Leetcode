98e035aa83de666f0c4cb6fe45cce0be61e451b9
koowinn <gooyunn@gmail.com>
2021-05-06 23:18:08 +0800

    Problem24:两两交换链表中的节点

---
+++ src/linkedlist/Problem24.java
@@ -0,0 +1,45 @@
+package linkedlist;
+
+/**
+ * 24. 两两交换链表中的节点
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ *
+ *
+ * 示例 1：
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ *
+ * 示例 2：
+ * 输入：head = []
+ * 输出：[]
+ *
+ * 示例 3：
+ * 输入：head = [1]
+ * 输出：[1]
+ *
+ *
+ * 提示：
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 <= Node.val <= 100
+ *
+ */
+public class Problem24 {
+    public ListNode swapPairs(ListNode head) {
+        // 定义一个哑节点
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode tmp = dummyNode;
+        while (tmp.next != null && tmp.next.next != null) {
+            ListNode node1 = tmp.next;
+            ListNode node2 = tmp.next.next;
+            tmp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            tmp = node1;
+        }
+        return dummyNode.next;
+    }
+}
