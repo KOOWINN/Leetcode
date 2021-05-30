baa6fba2021130d07c3fb2e12af2e1dd1788e943
koowinn <gooyunn@gmail.com>
2021-05-30 21:09:03 +0800

    Problem109:有序链表转换二叉搜索树

---
+++ src/tree/Problem109.java
@@ -0,0 +1,48 @@
+package tree;
+
+import linkedlist.ListNode;
+
+/**
+ * 109. 有序链表转换二叉搜索树
+ * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+ *
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ *
+ * 示例:
+ *
+ * 给定的有序链表： [-10, -3, 0, 5, 9],
+ *
+ * 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+ *
+ *       0
+ *      / \
+ *    -3   9
+ *    /   /
+ *  -10  5
+ */
+public class Problem109 {
+    public TreeNode sortedListToBST(ListNode head) {
+        return buildTree(head, null);
+    }
+
+    private TreeNode buildTree(ListNode left, ListNode right) {
+        if (left == right) {
+            return null;
+        }
+        ListNode mid = getMedian(left, right);
+        TreeNode root = new TreeNode(mid.val);
+        root.left = buildTree(left, mid);
+        root.right = buildTree(mid.next, right);
+        return root;
+    }
+
+    private ListNode getMedian(ListNode left, ListNode right) {
+        ListNode slow = left;
+        ListNode fast = left;
+        while (fast != right && fast.next != right) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
